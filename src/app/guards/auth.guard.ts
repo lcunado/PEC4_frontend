918cf5dbdf1655a6b884af1858cf9ebb592cd141
[Laura Cuñado García] <[lcunado@uoc.edu]>
2026-01-10 00:43:56 +0100

    Aplico guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserStoreService } from '../services/user-store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor( 
+    private userStore: UserStoreService, 
+    private router: Router 
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.userStore.isAuthenticated()) { 
+      return true; 
+    } 
+    return this.router.parseUrl('/login');
   }
   
 }
